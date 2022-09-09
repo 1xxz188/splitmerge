a4535093804d6906c2ce86c3b08e17ead43c049e
xiangzhixu <1009159531@qq.com>
2022-09-09 11:44:41 +0800

    修复文件大于2G不能读取问题

--- main.cc
+++ main.cc
@@ -7,31 +7,35 @@ int splitFile(const char* path, int count,  const char* savename)
 	FILE* F = fopen(path, "rb");
 	if (F == NULL)
 	{
+		printf("open file[%s] failed\n", path);
 		return  -1;
 	}
 	else
 	{
-		fseek(F, 0, SEEK_END);//将文件指针移动到文件末尾
-		int length = ftell(F);//计算文件指针到文件开头的字节数，即就是文件大小
-		int yushu = length % count;//余数
-		int size = length / count; //前面count-1份每一分大小为size，最后一份为size + yushu
-		for (int i = 1; i <= count; i++)
+		_fseeki64(F, 0L, SEEK_END);//将文件指针移动到文件末尾
+		__int64 length = _ftelli64(F);//计算文件指针到文件开头的字节数，即就是文件大小
+		__int64 yushu = length % count;//余数
+		__int64 size = length / count; //前面count-1份每一分大小为size，最后一份为size + yushu
+		printf("file length[%lld] per size[%lld]\n", length, size);
+
+		for (__int64 i = 1; i <= count; i++)
 		{
 			char savefile[100];
-			sprintf(savefile, "%s%d", savename,i);
+			sprintf(savefile, "%s%lld", savename,i);
 			printf("%s\n", savefile);
 			FILE* P = fopen(savefile, "wb");
 			if (P == NULL)
 			{
+				printf("open file[%s] failed\n", savefile);
 				fclose(F);
 				return -1;
 			}
 			else
 			{
-				fseek(F, (i - 1) * size, SEEK_SET);
+				_fseeki64(F, (i - 1) * size, SEEK_SET);
 				if (i == count)
 				{
-					for (int j = 0; j <= size + yushu; j++)
+					for (__int64 j = 0; j <= size + yushu; j++)
 					{
 						int c = fgetc(F);
 						fputc(c, P);
@@ -39,7 +43,7 @@ int splitFile(const char* path, int count,  const char* savename)
 				}
 				else
 				{
-					for (int j = 0; j < size; j++)
+					for (__int64 j = 0; j < size; j++)
 					{
 						int c = fgetc(F);
 						fputc(c, P);
@@ -57,19 +61,18 @@ int splitFile(const char* path, int count,  const char* savename)
 /*合并文件，合并为一个大文件bigfile返回-1表示分割失败,0表示成功*/
 int mergeFile(const char* savename, int count, const char* bigfile)
 {
-
 	FILE* BF = fopen(bigfile, "wb");
 	if (BF == NULL)
 	{
-		printf("open file failed");
+		printf("open file[%s] failed\n", savename);
 		return -1;
 	}
 	else
 	{
-		for (int i = 0; i < count; i++)
+		for (__int64 i = 0; i < count; i++)
 		{
 			char savefile[100];
-			sprintf(savefile, "%s%d",savename,i+1);
+			sprintf(savefile, "%s%lld",savename,i+1);
 
 			FILE* P = fopen(savefile, "rb");
 			if (P == NULL)
@@ -100,14 +103,19 @@ int main(int argc, char* argv[])
 		return 0;
 	}
 	if (strcmp(argv[1], "split") == 0) {
-
-		splitFile(argv[2], atoi(argv[3]), "funnywolf.split");
+		if (splitFile(argv[2], atoi(argv[3]), "funnywolf.split") == -1)
+			return -1;
 		printf("split file finish");
 		return 0;
 	}
 	else if (strcmp(argv[1], "merge") == 0) {
-		mergeFile("funnywolf.split", atoi(argv[3]), argv[2]);
+		if (mergeFile("funnywolf.split", atoi(argv[3]), argv[2]) == -1)
+			return -1;
 		printf("merge file finish");
 		return 0;
 	}
+	else {
+		printf("unkown argv\n");
+	}
+	return 0;
 }
